2f8f99aa78fa9655576b347d29f9bb56339bb264
vemv <vemv@users.noreply.github.com>
2019-02-19 14:48:10 +0100

    Expand `default-libspec-whitelist`

--- src/formatting_stack/formatters/clean_ns.clj
+++ src/formatting_stack/formatters/clean_ns.clj
@@ -30,7 +30,7 @@
             #{(str "^" x ".")
               (str "." x ".")
               (str "." x "$")})]
-    (->> ["specs" "imports" "exports" "extensions" "side-effects"]
+    (->> ["specs" "imports" "exports" "extensions" "side-effects" "init" "initialization" "load"]
          (mapcat variations)
          (into #{"^cljsjs."}))))
 
