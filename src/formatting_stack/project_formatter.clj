25fb5fbfeea12dca7776a2927d972d24fe87fb37
vemv <vemv@users.noreply.github.com>
2019-04-19 18:10:33 +0200

    Implement `formatting-stack.project-formatter`

---
+++ src/formatting_stack/project_formatter.clj
@@ -0,0 +1,44 @@
+(ns formatting-stack.project-formatter
+  "A set of defaults apt for formatting a whole project."
+  (:require
+   [formatting-stack.indent-specs]
+   [formatting-stack.core]
+   [formatting-stack.compilers.refactor-nrepl :as compilers.refactor-nrepl]
+   [formatting-stack.formatters.cider :as formatters.cider]
+   [formatting-stack.formatters.clean-ns :as formatters.clean-ns]
+   [formatting-stack.formatters.cljfmt :as formatters.cljfmt]
+   [formatting-stack.formatters.how-to-ns :as formatters.how-to-ns]
+   [formatting-stack.formatters.newlines :as formatters.newlines]
+   [formatting-stack.linters.bikeshed :as linters.bikeshed]
+   [formatting-stack.linters.eastwood :as linters.eastwood]
+   [formatting-stack.strategies :as strategies]))
+
+(def third-party-indent-specs formatting-stack.indent-specs/default-third-party-indent-specs)
+
+(def default-strategies [strategies/all-files])
+
+(def default-formatters
+  (let [opts {:third-party-indent-specs third-party-indent-specs}]
+    [(formatters.cider/map->Formatter {})
+     (formatters.cljfmt/map->Formatter opts)
+     (formatters.how-to-ns/map->Formatter (-> opts (assoc :strategies (conj default-strategies strategies/exclude-cljc))))
+     (formatters.newlines/map->Formatter opts)
+     (formatters.clean-ns/map->Formatter (-> opts (assoc :strategies (conj default-strategies
+                                                                           strategies/files-with-a-namespace
+                                                                           strategies/exclude-cljs
+                                                                           strategies/exclude-edn
+                                                                           strategies/do-not-use-cached-results!))))]))
+
+(def default-linters [(linters.bikeshed/map->Bikeshed {:strategies (conj default-strategies
+                                                                         strategies/exclude-edn)})
+                      (linters.eastwood/map->Eastwood {:strategies (conj default-strategies
+                                                                         strategies/exclude-cljs)})])
+
+(def default-compilers [(compilers.refactor-nrepl/map->Compiler {})])
+
+(defn format-and-lint-project! [& {:keys [in-background?]
+                                   :or   {in-background? (not (System/getenv "CI"))}}]
+  (formatting-stack.core/format! :strategies default-strategies
+                                 :formatters default-formatters
+                                 :linters default-linters
+                                 :in-background? in-background?))
