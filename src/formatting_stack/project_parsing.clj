8cd79c22f546b97f60a66750546271323813c559
vemv <vemv@users.noreply.github.com>
2019-06-11 07:58:51 +0200

    Parallelise `formatting-stack.project-parsing/project-namespaces`

--- src/formatting_stack/project_parsing.clj
+++ src/formatting_stack/project_parsing.clj
@@ -4,17 +4,23 @@
    [clojure.java.io :as io]
    [clojure.tools.namespace.file :as file]
    [clojure.tools.namespace.find :as find]
-   [clojure.tools.namespace.parse :as parse])
+   [clojure.tools.namespace.parse :as parse]
+   [nedap.utils.collections.eager :refer [partitioning-pmap]]
+   [nedap.utils.speced :as speced])
   (:import
    (java.io File)))
 
 (defn find-files [dirs platform]
   (->> dirs
        (map io/file)
-       (map #(.getCanonicalFile ^File %))
-       (filter #(.exists ^File %))
-       (mapcat #(find/find-sources-in-dir % platform))
-       (map #(.getCanonicalFile ^File %))))
+       (map (speced/fn [^File f]
+              (-> f .getCanonicalFile)))
+       (filter (speced/fn [^File f]
+                 (-> f .exists)))
+       (mapcat (speced/fn [^File f]
+                 (-> f (find/find-sources-in-dir platform))))
+       (map (speced/fn [^File f]
+              (-> f .getCanonicalFile)))))
 
 (defn project-namespaces
   "Returns all the namespaces contained or required in the current project.
@@ -22,11 +28,12 @@
   Includes third-party dependencies."
   []
   (->> (find-files (classpath/classpath-directories) find/clj)
-       (mapcat (fn [file]
-                 (let [decl (-> file file/read-file-ns-decl)
-                       n (-> decl parse/name-from-ns-decl)
-                       deps (-> decl parse/deps-from-ns-decl)]
-                   (conj deps n))))
+       (partitioning-pmap (fn [file]
+                            (let [decl (-> file file/read-file-ns-decl)
+                                  n (-> decl parse/name-from-ns-decl)
+                                  deps (-> decl parse/deps-from-ns-decl)]
+                              (conj deps n))))
+       (apply concat)
        (distinct)
        (filter identity)
        (keep find-ns)))
