dc06fd8a005fb5f19a43779dcb4ba443a2714386
vemv <vemv@users.noreply.github.com>
2019-07-11 08:59:32 +0200

    Setup Clojars credentials

--- project.clj
+++ project.clj
@@ -1,11 +1,21 @@
 (defproject formatting-stack "0.19.1"
   :description "An efficient, smart, graceful composition of formatters, linters and such."
+
   :url "https://github.com/nedap/formatting-stack"
-  :license {:name "Eclipse Public License"
-            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :license {:name "EPL-2.0"
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
+
+  :signing {:gpg-key "releases-staffingsolutions@nedap.com"}
+
   :repositories {"releases" {:url      "https://nedap.jfrog.io/nedap/staffing-solutions/"
                              :username :env/artifactory_user
                              :password :env/artifactory_pass}}
+
+  :deploy-repositories {"clojars" {:url      "https://clojars.org/repo"
+                                   :username :env/clojars_user
+                                   :password :env/clojars_pass}}
+
   :dependencies [[cljfmt "0.6.4" :exclusions [rewrite-clj]]
                  [com.gfredericks/how-to-ns "0.2.6"]
                  [com.gfredericks/lein-all-my-files-should-end-with-exactly-one-newline-character "0.1.1"]
@@ -22,6 +32,7 @@
                  [org.clojure/tools.reader "1.2.2"]
                  [refactor-nrepl "2.4.0"]
                  [rewrite-clj "0.6.1"]]
+
   :profiles {:dev      {:source-paths   ["dev"]
                         :resource-paths ["test-resources"]
                         :plugins        [[lein-cloverage "1.0.13"]]}
