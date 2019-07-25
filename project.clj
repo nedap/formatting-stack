a6048eb6f5ef3720d1361e094b1b309d84fedaa0
vemv <vemv@users.noreply.github.com>
2019-07-25 04:10:04 +0200

    v1.0.0-alpha1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject formatting-stack "0.19.3"
+(defproject formatting-stack "1.0.0-alpha1"
   :description "An efficient, smart, graceful composition of formatters, linters and such."
 
   :url "https://github.com/nedap/formatting-stack"
