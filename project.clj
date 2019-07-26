395d2093b2a826ffcb5355ed5af64f749cb5f618
vemv <vemv@users.noreply.github.com>
2019-07-26 13:31:43 +0200

    Bump some deps

--- project.clj
+++ project.clj
@@ -19,8 +19,8 @@
   :dependencies [[cljfmt "0.6.4" :exclusions [rewrite-clj]]
                  [com.gfredericks/how-to-ns "0.2.6"]
                  [com.gfredericks/lein-all-my-files-should-end-with-exactly-one-newline-character "0.1.1"]
-                 [com.nedap.staffing-solutions/speced.def "1.0.0-alpha3"]
-                 [com.nedap.staffing-solutions/utils.collections "2.0.0-alpha2"]
+                 [com.nedap.staffing-solutions/speced.def "1.0.0"]
+                 [com.nedap.staffing-solutions/utils.collections "2.0.0-alpha3"]
                  [com.stuartsierra/component "0.4.0"]
                  [integrant "0.7.0"]
                  [clj-kondo "2019.05.19-alpha"]
@@ -42,7 +42,7 @@
              :ci       {:plugins      [[cider/cider-nrepl "0.21.1"]]
                         :jvm-opts     ["-Dclojure.main.report=stderr"]
                         :global-vars  {*assert* true} ;; `ci.release-workflow` relies on runtime assertions
-                        :dependencies [[com.nedap.staffing-solutions/ci.release-workflow "1.3.0-alpha1"]]}
+                        :dependencies [[com.nedap.staffing-solutions/ci.release-workflow "1.3.0-alpha3"]]}
              ;; `dev` in :test is important - a test depends on it:
              :test     {:source-paths   ["dev"]
                         :resource-paths ["test-resources"]}})
