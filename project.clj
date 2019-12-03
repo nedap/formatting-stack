88ef98db3e85f33033a5439148908c6ae4f92435
Jeroen de Jong <jeroen.dejong@nedap.com>
2019-12-03 17:50:52 +0100

    v1.0.1

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 ;; Please don't bump the library version by hand - use ci.release-workflow instead.
-(defproject formatting-stack "1.0.0"
+(defproject formatting-stack "1.0.1"
   ;; Please keep the dependencies sorted a-z.
   :dependencies [[clj-kondo "2019.05.19-alpha"]
                  [cljfmt "0.6.5" :exclusions [rewrite-clj]]
