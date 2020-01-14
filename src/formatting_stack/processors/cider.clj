82624e54649734a3a036e60ff445cc4f94f1f8ce
vemv <vemv@users.noreply.github.com>
2020-01-14 04:46:31 +0100

    Spec and document the `cider` processor

--- src/formatting_stack/processors/cider.clj
+++ src/formatting_stack/processors/cider.clj
@@ -1,12 +1,19 @@
 (ns formatting-stack.processors.cider
   (:require
    [formatting-stack.protocols.processor :as processor]
-   [nedap.utils.modular.api :refer [implement]]))
+   [nedap.speced.def :as speced]
+   [nedap.utils.modular.api :refer [implement]]
+   [nedap.utils.spec.api :refer [check!]]))
 
-(defn process! [{:keys [third-party-indent-specs]} _files]
+(speced/defn process! [{:keys [^map? third-party-indent-specs]} _files]
   (doseq [[var-sym metadata] third-party-indent-specs]
+    (check! symbol? var-sym
+            map?    metadata)
     (some-> var-sym resolve (alter-meta! merge metadata))))
 
-(defn new [{:keys [third-party-indent-specs] :as options}]
+(defn new
+  "This processor alters var metadata from third-party libs,
+  so that runtime-based tooling such as CIDER can work more accurately."
+  [{:keys [third-party-indent-specs] :as options}]
   (implement options
     processor/--process! process!))
