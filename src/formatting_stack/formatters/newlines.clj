738aba15d5c4ae52c0b4a1cd501ff29c945f164a
Jeroen de Jong <jeroen.dejong@nedap.com>
2020-01-20 20:23:11 +0100

    Add newlines lint! option

--- src/formatting_stack/formatters/newlines.clj
+++ src/formatting_stack/formatters/newlines.clj
@@ -1,10 +1,14 @@
 (ns formatting-stack.formatters.newlines
   (:require
+   [clojure.string :as str]
    [com.gfredericks.all-my-files-should-end-with-exactly-one-newline-character :as impl]
    [formatting-stack.protocols.formatter :as formatter]
+   [formatting-stack.protocols.linter :as linter]
    [formatting-stack.util :refer [process-in-parallel!]]
    [nedap.speced.def :as speced]
-   [nedap.utils.modular.api :refer [implement]]))
+   [nedap.utils.modular.api :refer [implement]])
+  (:import
+   (java.io StringWriter)))
 
 (defn format! [{:keys [expected-newline-count]} files]
   (with-out-str ;; Supress "All newlines are good, nothing to fix."
@@ -13,8 +17,23 @@
                                  (impl/so-fix-them [filename] :expected-newline-count expected-newline-count)))))
   nil)
 
-(speced/defn new [{:keys [^pos-int? expected-newline-count]
-                   :or   {expected-newline-count 1}}]
+(defn lint! [{:keys [expected-newline-count]} files]
+  (binding [*out* (StringWriter.)
+            *err* *out*]
+    (->> files
+         (process-in-parallel! (fn [filename]
+                                 (when-not (zero? (impl/but-do-they? [filename] :expected-newline-count expected-newline-count))
+                                   {:filename filename
+                                    :source   :formatting-stack/newlines
+                                    :level    :warning
+                                    :line     (-> filename slurp (str/split "\n") count)
+                                    :msg      (str "File should end in " expected-newline-count " newlines")
+                                    :column   1})))
+         (remove nil?))))
+
+(speced/defn new [{:keys [^{::speced/spec #{0 1}} expected-newline-count]
+                   :or {expected-newline-count 1}}]
   (implement {:id ::id
               :expected-newline-count expected-newline-count}
+    linter/--lint! lint!
     formatter/--format! format!))
