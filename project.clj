349f6b25ab6f7c8dbdda687dab48f3fb076ac32a
vemv <vemv@users.noreply.github.com>
2020-02-08 13:53:14 +0100

    v3.0.0-alpha3

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 ;; Please don't bump the library version by hand - use ci.release-workflow instead.
-(defproject formatting-stack "3.0.0-alpha1"
+(defproject formatting-stack "3.0.0-alpha3"
   ;; Please keep the dependencies sorted a-z.
   :dependencies [[clj-kondo "2020.01.13"]
                  [cljfmt "0.6.5" :exclusions [rewrite-clj]]
