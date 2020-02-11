b1c488bbe66fdc1ca3a0ec836db192910d22d415
vemv <vemv@users.noreply.github.com>
2020-02-11 02:15:04 +0100

    Remove a disabled test
    It would be anomalous that Eastwood hit a syntactically broken anyway - it could indicate room for improvement in strategies/etc.

--- test/functional/formatting_stack/linters/eastwood.clj
+++ test/functional/formatting_stack/linters/eastwood.clj
@@ -10,15 +10,6 @@
   (let [linter (sut/new {})]
     (are [filename expected] (match? expected
                                      (linter/lint! linter [filename]))
-      ;; fixme should return a :reader-exception, currently lost in :note. lint! yields empty list
-      #_#_"test-resources/invalid_syntax.clj"
-        (matchers/embeds
-         [{:level    :exception,
-           :filename "test-resources/invalid_syntax.clj",
-           :line     3,
-           :column   2,
-           :source   :eastwood/lint!}])
-
       "test-resources/eastwood_warning.clj"
       (matchers/embeds
        [{:source   :eastwood/def-in-def
