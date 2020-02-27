24584533794dc480b6e652050e378587dc089139
vemv <vemv@users.noreply.github.com>
2020-02-27 11:22:32 +0100

    Complete `integration.formatting-stack.strategies`

--- test/integration/formatting_stack/strategies.clj
+++ test/integration/formatting_stack/strategies.clj
@@ -1,7 +1,10 @@
 (ns integration.formatting-stack.strategies
   (:require
-   [clojure.test :refer :all]
-   [formatting-stack.strategies :as sut]))
+   [clojure.java.shell :refer [sh]]
+   [clojure.test :refer [deftest is testing]]
+   [formatting-stack.strategies :as sut])
+  (:import
+   (java.io File)))
 
 (deftest all-files
   (testing "It runs without errors, exercising its specs"
@@ -15,3 +18,27 @@
           n (->> (sut/git-diff-against-default-branch :target-branch first-commit)
                  (count))]
       (is (pos? n)))))
+
+(def ^File deletable-file (File. "test-resources/deletable.clj"))
+
+(assert (-> deletable-file .exists))
+
+(deftest git-not-completely-staged
+  (testing "It can gracefully handle the presence of files partially staged for deletion"
+    (try
+      (-> deletable-file .delete)
+      (is (sut/git-not-completely-staged :files []))
+      (finally
+        (sh "git" "checkout" (str deletable-file))
+        (assert (-> deletable-file .exists))))))
+
+(deftest git-completely-staged
+  (testing "It can gracefully handle the presence of files staged for deletion"
+    (try
+      (-> deletable-file .delete)
+      (sh "git" "add" (str deletable-file))
+      (is (sut/git-completely-staged :files []))
+      (finally
+        (sh "git" "reset" "--" (str deletable-file))
+        (sh "git" "checkout" (str deletable-file))
+        (assert (-> deletable-file .exists))))))
