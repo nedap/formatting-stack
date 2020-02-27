c0f56ace20bda4f8fb68ce4db1c6f4a6043c6691
vemv <vemv@users.noreply.github.com>
2020-02-28 00:52:27 +0100

    Expand integration.formatting-stack.strategies

--- test/integration/formatting_stack/strategies.clj
+++ test/integration/formatting_stack/strategies.clj
@@ -6,24 +6,60 @@
   (:import
    (java.io File)))
 
+(defn expect-sane-output! [corpus]
+  (let [n (count corpus)]
+    (is (pos? n))
+    (doseq [^String filename corpus
+            :let [f (File. filename)]]
+      (is (-> f .exists))
+      (testing "It emits absolutized filenames"
+        (is (= filename
+               (-> f .getCanonicalPath)))))))
+
 (deftest all-files
   (testing "It runs without errors, exercising its specs"
-    (let [n (->> (sut/all-files :files [])
-                 (count))]
-      (is (pos? n)))))
+
+    (expect-sane-output! (sut/all-files :files []))))
 
 (deftest git-diff-against-default-branch
   (testing "It runs without errors, exercising its specs"
-    (let [first-commit "ebb0ca8" ;; f-stack's first ever commit
-          n (->> (sut/git-diff-against-default-branch :target-branch first-commit)
-                 (count))]
-      (is (pos? n)))))
+    (let [first-commit "ebb0ca8" ;; f-stack's first ever commit. This ensures a large, diverse corpus
+          corpus (sut/git-diff-against-default-branch :target-branch first-commit)]
+
+      (expect-sane-output! (sut/git-diff-against-default-branch :target-branch first-commit)))))
 
 (def ^File deletable-file (File. "test-resources/deletable.clj"))
 
+(def ^String createable-filename "test-resources/createable.clj")
+
+(def creatable-contents (pr-str '(ns foo)))
+
 (assert (-> deletable-file .exists))
 
+(assert (not (-> createable-filename File. .exists)))
+
+;; NOTE: naturally, this test can fail if your git stataus contains any changes.
+;; You may need to `git commit` your WIP for these tests to pass.
 (deftest git-not-completely-staged
+
+  (testing "Non-completely-added files show up"
+    (try
+      (spit createable-filename creatable-contents)
+
+      (is (= [(-> createable-filename File. .getCanonicalPath)]
+             (sut/git-not-completely-staged :files [])))
+
+      (testing "Added files don't show up"
+        (try
+          (sh "git" "add" "-A")
+          (is (= []
+                 (sut/git-not-completely-staged :files [])))
+          (finally
+            (sh "git" "reset" "."))))
+
+      (finally
+        (-> createable-filename File. .delete))))
+
   (testing "It can gracefully handle the presence of files partially staged for deletion"
     (try
       (-> deletable-file .delete)
@@ -32,7 +68,28 @@
         (sh "git" "checkout" (str deletable-file))
         (assert (-> deletable-file .exists))))))
 
+;; NOTE: naturally, this test can fail if your git stataus contains any changes.
+;; You may need to `git commit` your WIP for these tests to pass.
 (deftest git-completely-staged
+
+  (testing "Non-ompletely-added files don't show up"
+    (try
+      (spit createable-filename creatable-contents)
+
+      (is (= []
+             (sut/git-completely-staged :files [])))
+
+      (testing "Added files show up"
+        (try
+          (sh "git" "add" "-A")
+          (is (= [(-> createable-filename File. .getCanonicalPath)]
+                 (sut/git-completely-staged :files [])))
+          (finally
+            (sh "git" "reset" "."))))
+
+      (finally
+        (-> createable-filename File. .delete))))
+
   (testing "It can gracefully handle the presence of files staged for deletion"
     (try
       (-> deletable-file .delete)
