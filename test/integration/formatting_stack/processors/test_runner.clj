0dfe758df0425a94359a2ff1dd634666051f0a8f
Jeroen de Jong <jeroen.dejong@nedap.com>
2020-03-12 13:03:33 +0100

    Add test

---
+++ test/integration/formatting_stack/processors/test_runner.clj
@@ -0,0 +1,11 @@
+(ns integration.formatting-stack.processors.test-runner
+  (:require
+   [formatting-stack.processors.test-runner :as sut]
+   [clojure.test :refer :all]))
+
+
+(deftest test!
+  (testing "asserts *load-tests* is true"
+    (binding [*load-tests* false]
+      (is (thrown-with-msg? AssertionError #"clojure.test/\*load-tests\*"
+                            (sut/test!))))))
