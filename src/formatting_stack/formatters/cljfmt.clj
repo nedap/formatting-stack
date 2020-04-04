86e5c2329caa396f71889629a8a94a8e667cbafc
Jeroen de Jong <jeroen.dejong@nedap.com>
2020-04-04 22:17:13 +0200

    Add cljfmt lint! test

--- src/formatting_stack/formatters/cljfmt.clj
+++ src/formatting_stack/formatters/cljfmt.clj
@@ -2,9 +2,10 @@
   (:require
    [cljfmt.main]
    [formatting-stack.formatters.cljfmt.impl :as impl]
+   [formatting-stack.indent-specs :refer [default-third-party-indent-specs]]
    [formatting-stack.protocols.formatter :as formatter]
    [formatting-stack.protocols.linter :as linter]
-   [formatting-stack.util :refer [process-in-parallel!]]
+   [formatting-stack.util :refer [ensure-sequential process-in-parallel!]]
    [nedap.speced.def :as speced]
    [nedap.utils.modular.api :refer [implement]]))
 
@@ -18,21 +19,26 @@
 (defn lint! [{:keys [third-party-indent-specs]} files]
   (->> files
        (process-in-parallel! (fn [filename]
-                               (let [indents (impl/cljfmt-indents-for filename third-party-indent-specs)]
-                                 (#'cljfmt.main/check-one {:indents indents} filename))))
-       (filter (fn [{{:keys [okay]} :counts}] (zero? okay)))
-       (mapcat (fn [{:keys [file diff]}]
-                 (->> diff
-                   (re-seq #".*\@\@\ -(\d),(\d) \+\d,\d \@\@*") ;; fixme
-                   (map #(take-last 2 %))
-                   (map (fn [[start stop]]
-                          {:filename file
-                           :diff diff
-                           :level :warning
-                           :msg (str "Indentation is wrong between " start "-" stop)
-                           :source :cljfmt/indent})))))))
+                               (let [indents (impl/cljfmt-indents-for filename third-party-indent-specs)
+                                     {{:keys [okay]} :counts
+                                      :keys [file diff]} (#'cljfmt.main/check-one {:indents indents} filename)]
+                                 (when (zero? okay)
+                                   (->> diff
+                                        (re-seq #".*\@\@\ -(\d),(\d) \+\d,\d \@\@*") ;; fixme
+                                        (map #(take-last 2 %))
+                                        (mapv (fn [[start stop]]
+                                                {:filename file
+                                                 :diff diff
+                                                 :level :warning
+                                                 :column 1
+                                                 :line (Long/parseLong start)
+                                                 :msg (str "Indentation is wrong between " start "-" stop)
+                                                 :source :cljfmt/indent})))))))
+       (mapcat ensure-sequential)))
 
-(speced/defn new [{:keys [third-party-indent-specs] :as options}]
-  (implement (assoc options :id ::id)
+(speced/defn new [{:keys [third-party-indent-specs]
+                   :or {third-party-indent-specs default-third-party-indent-specs}}]
+  (implement {:id ::id
+              :third-party-indent-specs third-party-indent-specs}
     formatter/--format! format!
     linter/--lint! lint!))
