1b6e58c95126fd7a9b9edad1e2f124519627d2e1
Jeroen de Jong <jeroen.dejong@nedap.com>
2020-04-05 13:31:18 +0200

    fix another assertion

--- test/functional/formatting_stack/formatters/cljfmt.clj
+++ test/functional/formatting_stack/formatters/cljfmt.clj
@@ -17,6 +17,6 @@
       "test-resources/wrong_indent.clj"
       [{:source :cljfmt/indent
         :msg "Indentation is wrong between 4-7"
-        :line 0
+        :line 4
         :column 0
         :filename "test-resources/wrong_indent.clj"}])))
