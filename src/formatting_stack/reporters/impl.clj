3a9c4a78731b6be0c1a65989dafb4eb9149e856c
Jeroen de Jong <jeroen.dejong@nedap.com>
2020-05-05 16:55:22 +0200

    Account for nillable filenames

--- src/formatting_stack/reporters/impl.clj
+++ src/formatting_stack/reporters/impl.clj
@@ -1,12 +1,13 @@
 (ns formatting-stack.reporters.impl
   (:require
    [clojure.string :as string]
+   [medley.core :refer [assoc-some]]
    [nedap.speced.def :as speced]))
 
 (speced/defn normalize-filenames
   "Removes the CWD from the filenames, for more concise output, and also for ensuring correct grouping."
-  [{:keys [^string? filename] :as report}]
-  (assoc report :filename (-> filename
-                              (string/replace (re-pattern (str "^" (System/getProperty "user.dir")))
-                                              "")
-                              (string/replace #"^/" ""))))
+  [{:keys [^::speced/nilable ^string? filename] :as report}]
+  (assoc-some report :filename (some-> filename
+                                       (string/replace (re-pattern (str "^" (System/getProperty "user.dir")))
+                                                       "")
+                                       (string/replace #"^/" ""))))
