6853b8d9be4f328f03b6ad6e5eada63ebb2cb088
Jeroen de Jong <jeroen.dejong@nedap.com>
2020-05-05 16:52:05 +0200

    Add failing test-case for exceptions in core/format!

---
+++ test/functional/formatting_stack/core.clj
@@ -0,0 +1,37 @@
+(ns functional.formatting-stack.core
+  (:require
+   [clojure.test :refer :all]
+   [formatting-stack.core :as sut]
+   [formatting-stack.protocols.formatter :as formatter]
+   [formatting-stack.protocols.reporter :as protocols.reporter]
+   [matcher-combinators.test :refer [match?]]
+   [nedap.utils.modular.api :refer [implement]])
+  (:import (clojure.lang ExceptionInfo)))
+
+(defn throw-exception [& args]
+  (throw (ex-info "Kaboom!" {})))
+
+(def failing-formatter
+  (implement {:id ::failing-formatter}
+    formatter/--format! throw-exception))
+
+(defn store-in-latest [{::keys [latest]} args]
+  (reset! latest args))
+
+(defn recording-recorder [latest]
+  (implement {::latest latest}
+    protocols.reporter/--report store-in-latest))
+
+(deftest format!
+  (testing "exceptions are passed to the reporter"
+    (let [x (atom nil)]
+      (sut/format! :formatters [failing-formatter]
+                   :linters []
+                   :processors []
+                   :reporter (recording-recorder x)
+                   :in-background? false)
+      (is (match? [{:source    :formatting-stack/process!
+                    :msg       "Exception during {:id :functional.formatting-stack.core/failing-formatter}"
+                    :level     :exception
+                    :exception #(instance? ExceptionInfo %)}]
+                  @x)))))
