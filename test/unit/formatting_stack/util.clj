12aeb31a5935543933e3bdcb7641a27e51b020eb
Jeroen de Jong <jeroen.dejong@nedap.com>
2020-05-05 12:05:30 +0200

    Replace grouping-spec and add testcase

--- test/unit/formatting_stack/util.clj
+++ test/unit/formatting_stack/util.clj
@@ -20,37 +20,40 @@
                                          (is (= expected
                                                 (sut/diff->line-numbers (slurp filename))))
                                          true)
+
     "only additions yields empty result"
     "test-resources/diffs/1.patch"
-    ()
+    []
 
-    "patch with 3 sections for one file"
+    "3 sections for one file"
     "test-resources/diffs/2.patch"
     [{:begin    14
       :end      14
-      :filename "b/src/formatting_stack/formatters/trivial_ns_duplicates.clj"}
+      :filename "src/formatting_stack/formatters/trivial_ns_duplicates.clj"}
      {:begin    144
       :end      144
-      :filename "b/src/formatting_stack/formatters/trivial_ns_duplicates.clj"}
+      :filename "src/formatting_stack/formatters/trivial_ns_duplicates.clj"}
      {:begin    146
       :end      146
-      :filename "b/src/formatting_stack/formatters/trivial_ns_duplicates.clj"}
+      :filename "src/formatting_stack/formatters/trivial_ns_duplicates.clj"}
      {:begin    154
       :end      154
-      :filename "b/src/formatting_stack/formatters/trivial_ns_duplicates.clj"}]
+      :filename "src/formatting_stack/formatters/trivial_ns_duplicates.clj"}]
 
-    "patch with multiple changed lines in one section"
+    "multiple changed lines in one section"
     "test-resources/diffs/3.patch"
     [{:begin    12
       :end      12
-      :filename "b/src/formatting_stack/formatters/no_extra_blank_lines.clj"}
+      :filename "src/formatting_stack/formatters/no_extra_blank_lines.clj"}
      {:begin    30
       :end      33
-      :filename "b/src/formatting_stack/formatters/no_extra_blank_lines.clj"}
+      :filename "src/formatting_stack/formatters/no_extra_blank_lines.clj"}
      {:begin    41
       :end      41
-      :filename "b/src/formatting_stack/protocols/spec.clj"}]
+      :filename "src/formatting_stack/protocols/spec.clj"}]
 
-    "patch with only additions"
+    "moved code"
     "test-resources/diffs/4.patch"
-    []))
+    [{:begin    5
+      :end      5
+      :filename "test/unit/formatting_stack/strategies.clj"}]))
