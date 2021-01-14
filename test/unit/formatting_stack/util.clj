0719e3b7d677d3ad6e46e0cd2a535e3369403ecd
Jeroen de Jong <jeroen.dejong@nedap.com>
2021-01-14 23:32:43 +0100

    Add test for process-in-parallel

--- test/unit/formatting_stack/util.clj
+++ test/unit/formatting_stack/util.clj
@@ -1,7 +1,9 @@
 (ns unit.formatting-stack.util
   (:require
+   [clojure.string :as str]
    [clojure.test :refer :all]
-   [formatting-stack.util :as sut]))
+   [formatting-stack.util :as sut]
+   [matcher-combinators.test :refer [match?]]))
 
 (deftest read-ns-decl
   (are [input expected] (= expected
@@ -15,3 +17,21 @@
     "test-resources/sample_cljs_ns.cljs" '(ns sample-cljs-ns
                                             (:require [foo.bar.baz :as baz])
                                             (:require-macros [sample-cljs-ns :refer [the-macro]]))))
+
+(deftest process-in-parallel!
+  (testing "error reporting"
+    (are [f xs expected] (match? expected
+                                 (sut/process-in-parallel! f xs))
+      (fn [_] (throw (ex-info "expected" {})))
+      ["made_up_name.clj"]
+      [{:exception #(= "expected" (ex-message %))
+        :level     :exception
+        :filename  "made_up_name.clj"
+        :msg       #(str/starts-with? % "Encountered an exception while running")}]
+
+      (fn [_] (assert false "expected"))
+      ["made_up_name.clj"]
+      [{:exception #(= "Assert failed: expected\nfalse" (ex-message %))
+        :level     :exception
+        :filename  "made_up_name.clj"
+        :msg       #(str/starts-with? % "Encountered an exception while running")}])))
