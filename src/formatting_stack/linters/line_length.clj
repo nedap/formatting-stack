767a4f105da8f1fa3b8ea9bb7883f9c8bde25b5e
Jeroen de Jong <jeroen.dejong@nedap.com>
2021-02-23 15:56:05 +0100

    Make line-length linter less noisy
    Adds merge-threshold option which merges line-length errors which are close to eachother (probable to be the same code-block).
    The old behaviour is still configurable by passing 0 as the threshold

--- src/formatting_stack/linters/line_length.clj
+++ src/formatting_stack/linters/line_length.clj
@@ -5,26 +5,36 @@
    [formatting-stack.util :refer [ensure-sequential process-in-parallel!]]
    [nedap.utils.modular.api :refer [implement]]))
 
-(defn exceeding-lines [threshold filename]
-  (->> (-> filename slurp (string/split #"\n"))
+(defn exceeding-lines [{:keys [max-line-length merge-threshold]} filename]
+  (->> (-> filename slurp string/split-lines)
        (map-indexed (fn [i row]
                       (let [column-count (count row)]
-                        (when (< threshold column-count)
+                        (when (< max-line-length column-count)
                           {:filename filename
                            :source   :formatting-stack/line-length
                            :level    :warning
                            :column   column-count
                            :line     (inc i)
-                           :msg      (str "Line exceeding " threshold " columns.")}))))
-       (filterv some?)))
+                           :msg      (str "Line exceeding " max-line-length " columns.")}))))
+       (filter some?)
+       ;; remove duplicates within `merge-threshold` lines
+       (reduce (fn [memo {:keys [line] :as report}]
+                 (let [diff (- (or line 0)
+                               (or (:line (last memo)) 0))]
+                   (if (< diff merge-threshold)
+                     (conj (vec (butlast memo)) report)
+                     (conj memo report))))
+               [])))
 
-(defn lint! [{:keys [max-line-length]} filenames]
+(defn lint! [options filenames]
   (->> filenames
-       (process-in-parallel! (partial exceeding-lines max-line-length))
+       (process-in-parallel! (partial exceeding-lines options))
        (mapcat ensure-sequential)))
 
-(defn new [{:keys [max-line-length]
-            :or   {max-line-length 130}}]
+(defn new [{:keys [max-line-length merge-threshold]
+            :or   {max-line-length 130
+                   merge-threshold 10}}]
   (implement {:id ::id
+              :merge-threshold merge-threshold
               :max-line-length max-line-length}
     linter/--lint! lint!))
