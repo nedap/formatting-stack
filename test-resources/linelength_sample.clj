5c5c60c3b5568421c69db9505ae7bc8b5d8f27a7
Jeroen de Jong <jeroen.dejong@nedap.com>
2021-04-01 10:21:57 +0200

    increase test coverage

--- test-resources/linelength_sample.clj
+++ test-resources/linelength_sample.clj
@@ -1,7 +1,24 @@
 (ns linelength-sample)
 
-;; reports should be grouped
+[1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1] ;; should be grouped
+; intentional newline
+; intentional newline
+; intentional newline
+; intentional newline
+; intentional newline
+; intentional newline
+; intentional newline
+; intentional newline
+; intentional newline
+[1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1] ;; should be grouped
 [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1]
-
+; intentional newline
+; intentional newline
+; intentional newline
+; intentional newline
+; intentional newline
+; intentional newline
+; intentional newline
+[1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1] ;; should be grouped
 [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1]
 [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1]
